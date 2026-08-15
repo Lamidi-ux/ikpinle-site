887ea8917fceef054fe14778cb8d4271bd537f4a
Lamidi-ux <moussilioulamidi449@gmail.com>
2026-08-15 13:08:44 +0100

    Add files via upload

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,1 +1,10 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext.jsx";
 
+export function useAuth() {
+  const contexte = useContext(AuthContext);
+  if (!contexte) {
+    throw new Error("useAuth doit être utilisé à l'intérieur d'un <AuthProvider>.");
+  }
+  return contexte;
+}
